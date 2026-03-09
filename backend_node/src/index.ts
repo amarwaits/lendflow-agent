863fc0c8c1f52c40758a6dc1ba4fe566f830f712
Amar Kumar <amar2020agarwal@gmail.com>
2026-03-10 03:02:19 +0530

    Bug fixes

--- backend_node/src/index.ts
+++ backend_node/src/index.ts
@@ -1,4 +1,6 @@
-import 'dotenv/config';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 import { initDb } from './db';
 import { createApp } from './app';
 
